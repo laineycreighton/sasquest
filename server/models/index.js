45bcb843e7c8254e0185705b7a51288bd07dff21
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-08 13:39:51 -0500

    pseudocode for server side + starter code

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Project = require("./Project");
+const Info = require("./Info");
+const Timeline = require("./Timeline");
+const Wireframe = require("./Wireframe");
+
+module.exports = { User, Project, Info, Timeline, Wireframe };
