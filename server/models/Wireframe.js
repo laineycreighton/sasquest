c34db5b409a24f9d02d270eae62006cfa78ce604
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-08 18:18:44 -0500

    code for Wireframe.js models

--- server/models/Wireframe.js
+++ server/models/Wireframe.js
@@ -2,6 +2,30 @@ const { Schema, model } = require("mongoose");
 
 const wireframeSchema = new Schema({
   // TODO: Add your schema here
+  //   Id
+  // ProjectId
+  // Title
+  // ImageUrl (Cloudanary)
+  // Note
+
+  projectId: {
+    type: Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+  note: {
+    type: String,
+    required: false,
+  },
 });
 
 const Wireframe = model("Wireframe", wireframeSchema);
