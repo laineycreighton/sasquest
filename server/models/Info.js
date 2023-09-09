ca608af0f3e71855d975e1190efbe3ba144e88df
Lainey <dev.lainey@gmail.com>
2023-09-08 21:26:42 -0400

    update Project model

--- server/models/Info.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const infoSchema = new Schema({
-  // TODO: Add your schema here
-});
-
-const Info = model("Info", infoSchema);
-
-module.exports = Info;
--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,4 @@
 const User = require("./User");
 const Project = require("./Project");
-const Info = require("./Info");
-const Timeline = require("./Timeline");
-const Wireframe = require("./Wireframe");
 
-module.exports = { User, Project, Info, Timeline, Wireframe };
+module.exports = { User, Project };
