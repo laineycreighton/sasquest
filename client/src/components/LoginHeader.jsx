dd5c4ed75d264fdd574d52ba59f4407e393a2c72
Lainey <dev.lainey@gmail.com>
2023-09-09 00:41:58 -0400

    add LoginHeader to client side

--- client/src/components/LoginHeader.jsx
+++ client/src/components/LoginHeader.jsx
@@ -1,14 +1,21 @@
-// BELONGS TO MANY:
-//      - Login.jsx
-//      - Register.jsx
-//
-//
-// FUNCTIONALITY:
-//      - link to about page:
-//                      01. About.jsx
-//
-//
-// VISUAL:
-//      - renders a SASQUEST logo
-//                               01. logo
-//                               02. tag line
+import { Link } from 'react-router-dom'
+import '../styles/LoginHeader.css';
+
+//Export the LoginHeader page
+export default function LoginHeader() {
+    return (
+        <div>
+            <div className='redirect-about'>
+                <Link to="/about">About</Link>
+            </div>
+            <div className='header'>
+                <header className='sasquest-name'>
+                    <h1><span>S</span>ASQUES<span>T</span></h1>
+                </header>
+                <p className='tagline'>
+                    tame your beast
+                </p>
+            </div>
+        </div>
+    )
+}
