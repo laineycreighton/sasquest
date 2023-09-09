49dc6a55d043952b1f3477347a37bcf2454e5a59
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-09 12:38:28 -0500

    added update and delete wireframe for mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,7 @@
 import { gql } from "@apollo/client";
 
+//-------------------- WIREFRAME --------------------//
+// Create Wireframe
 export const CREATE_WIREFRAME = gql`
   mutation createWireframe(
     $projectId: ID!
@@ -21,11 +23,57 @@ export const CREATE_WIREFRAME = gql`
   }
 `;
 
+// Update Wireframe
+export const UPDATE_WIREFRAME = gql`
+  mutation updateWireframe(
+    $projectId: ID!
+    $wireframeId: ID!
+    $title: String!
+    $imageUrl: String
+    $note: String
+  ) {
+    updateWireframe(
+      projectId: $projectId
+      wireframeId: $wireframeId
+      title: $title
+      imageUrl: $imageUrl
+      note: $note
+    ) {
+      _id
+      projectId
+      imageUrl
+      note
+    }
+  }
+`;
+
+// Delete Wireframe
+export const DELETE_WIREFRAME = gql`
+  mutation deleteWireframe($projectId: ID!, $wireframeId: ID!) {
+    deleteWireframe(projectId: $projectId, wireframeId: $wireframeId) {
+      _id
+      projectId
+      imageUrl
+      note
+    }
+  }
+`;
+
 //-------------------- INFO --------------------//
 // Add Info
 export const ADD_INFO = gql`
-  mutation addInfo($projectId: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    addInfo(projectId: $projectId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation addInfo(
+    $projectId: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    addInfo(
+      projectId: $projectId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
       _id
       title
       info {
@@ -51,8 +99,20 @@ export const ADD_INFO = gql`
 
 // Update Info
 export const UPDATE_INFO = gql`
-  mutation updateInfo($projectId: ID!, $infoID: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    updateInfo(projectId: $projectId, infoId: $infoId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation updateInfo(
+    $projectId: ID!
+    $infoID: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    updateInfo(
+      projectId: $projectId
+      infoId: $infoId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
       _id
       title
       info {
@@ -78,8 +138,20 @@ export const UPDATE_INFO = gql`
 
 // Delete Info
 export const DELETE_INFO = gql`
-  mutation updateInfo($projectId: ID!, $infoID: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    updateInfo(projectId: $projectId, infoId: $infoId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation updateInfo(
+    $projectId: ID!
+    $infoID: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    updateInfo(
+      projectId: $projectId
+      infoId: $infoId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
       _id
       title
       info {
