5a27ab3f7ab5593b48ba13639baac2db95b1ace9
lukeharriman <lukeharriman1013@gmail.com>
2023-09-09 13:21:52 -0700

    index, client and server mutations for project model, project model

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,13 @@
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
+User.hasMany(Project, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Project.belongsTo(User, {
+    foreignKey: "user_id",
+    });
+
 module.exports = { typeDefs, resolvers };
