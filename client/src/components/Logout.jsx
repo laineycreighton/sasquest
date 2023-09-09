03e005f8d1538727033ddcbf66f4bdea387d46f4
Lainey <dev.lainey@gmail.com>
2023-09-09 11:00:21 -0400

    add Logout component

---
+++ client/src/components/Logout.jsx
@@ -0,0 +1,33 @@
+//-------------------- IMPORT --------------------//
+
+//----- React useHistory -----//
+import { useHistory } from 'react-router-dom';
+
+//----- User Authentication -----//
+import Auth from '../utils/auth';
+
+//----- CSS -----//
+import './styles/Logout.css'
+
+
+
+//-------------------- LOGIC --------------------//
+const history = useHistory();
+
+  const handleLogout = () => {
+//TODO: write logic to logout a user
+
+//TODO: redirect the user to the login page ('/' - path)
+  };
+
+
+
+//-------------------- EXPORT --------------------//
+
+//----- Export the Logout Button -----//
+export default function Logout() {
+    return (
+        //----- Log Out Button -----//
+        <button onClick={handleLogout}>Log Out</button>
+      );
+}
