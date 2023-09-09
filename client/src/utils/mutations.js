5fc8105fba5c87bfff953bbc0c19365ac7672731
lukeharriman <lukeharriman1013@gmail.com>
2023-09-09 12:47:09 -0700

    createProject, deleteProject, updateProject mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,8 +24,18 @@ export const CREATE_WIREFRAME = gql`
 //-------------------- INFO --------------------//
 // Add Info
 export const ADD_INFO = gql`
-  mutation addInfo($projectId: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    addInfo(projectId: $projectId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation addInfo(
+    $projectId: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    addInfo(
+      projectId: $projectId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
       _id
       title
       info {
@@ -51,8 +61,20 @@ export const ADD_INFO = gql`
 
 // Update Info
 export const UPDATE_INFO = gql`
-  mutation updateInfo($projectId: ID!, $infoID: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    updateInfo(projectId: $projectId, infoId: $infoId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation updateInfo(
+    $projectId: ID!
+    $infoID: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    updateInfo(
+      projectId: $projectId
+      infoId: $infoId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
       _id
       title
       info {
@@ -78,8 +100,106 @@ export const UPDATE_INFO = gql`
 
 // Delete Info
 export const DELETE_INFO = gql`
-  mutation updateInfo($projectId: ID!, $infoID: ID!, $repoURL: String!, $deployedURL: String!, $description: String!) {
-    updateInfo(projectId: $projectId, infoId: $infoId, repoURL: $repoURL, deployedURL: $deployedURL, description: $description) {
+  mutation updateInfo(
+    $projectId: ID!
+    $infoId: ID!
+    $repoURL: String!
+    $deployedURL: String!
+    $description: String!
+  ) {
+    updateInfo(
+      projectId: $projectId
+      infoId: $infoId
+      repoURL: $repoURL
+      deployedURL: $deployedURL
+      description: $description
+    ) {
+      _id
+      title
+      info {
+        _id
+        repoURL
+        deployedURL
+        description
+      }
+      timeline {
+        _id
+        date
+        goal
+      }
+      wireframe {
+        _id
+        title
+        imageURL
+        note
+      }
+    }
+  }
+`;
+
+//-------------------- PROJECT --------------------//
+
+// Add Project
+//createProject has arguments title and projectId
+export const ADD_PROJECT = gql`
+  mutation createProject($title: String!, $projectId: ID!) {
+    createProject(title: $title, projectId: $projectId) {
+      _id
+      title
+      info {
+        _id
+        repoURL
+        deployedURL
+        description
+      }
+      timeline {
+        _id
+        date
+        goal
+      }
+      wireframe {
+        _id
+        title
+        imageURL
+        note
+      }
+    }
+  }
+`;
+
+// Update Project has arguments projectid and title
+
+export const UPDATE_PROJECT = gql`
+  mutation updateProject($projectId: ID!, $title: String!) {
+    updateProject(projectId: $projectId, title: $title) {
+      _id
+      title
+      info {
+        _id
+        repoURL
+        deployedURL
+        description
+      }
+      timeline {
+        _id
+        date
+        goal
+      }
+      wireframe {
+        _id
+        title
+        imageURL
+        note
+      }
+    }
+  }
+`;
+
+// Delete Project has arguments project id and title
+
+export const DELETE_PROJECT = gql`
+  mutation deleteProject($projectId: ID!, $title: String!) {
+    deleteProject(projectId: $projectId, title: $title) {
       _id
       title
       info {
