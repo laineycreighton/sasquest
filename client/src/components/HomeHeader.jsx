2166985cd65dd028ff648dab0adfcc86771c6060
Lainey <dev.lainey@gmail.com>
2023-09-09 00:44:29 -0400

    add HomeHeader

--- client/src/components/HomeHeader.jsx
+++ client/src/components/HomeHeader.jsx
@@ -12,3 +12,24 @@
 //      - renders the SASQUEST logo with tagline
 //                               01. logo
 //                               02. tagline
+import { Link } from 'react-router-dom'
+import '../styles/HomeHeader.css';
+
+//Export the HomeHeader page
+export default function HomeHeader() {
+    return (
+        <div className='home-header'>
+            <div className='redirect-about'>
+                <Link to="/login">Logout</Link>
+            </div>
+            <div className='header'>
+                <header className='sasquest-name'>
+                    <h1><span>S</span>ASQUES<span>T</span></h1>
+                </header>
+                <p className='tagline'>
+                    tame your beast
+                </p>
+            </div>
+        </div>
+    )
+}
