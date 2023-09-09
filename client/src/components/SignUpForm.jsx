73278a9046f24ea7cf1106d117d8aa31228eab3b
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-09 13:39:30 -0500

    sign-form

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -12,3 +12,69 @@
 //                               02. last name
 //                               03. email
 //                               04. password
+
+import { useEffect, useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+const SignUpForm = () => {
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  // use mutation for adding user
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setUserFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return;
+};
+
+export default SignUpForm;
