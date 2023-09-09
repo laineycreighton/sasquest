8073566ccc386c099b038071594375a78278d8ed
Lainey <dev.lainey@gmail.com>
2023-09-09 00:19:40 -0400

    add EditInfo.jsx

--- client/src/pages/EditInfo.jsx
+++ client/src/pages/EditInfo.jsx
@@ -11,3 +11,23 @@
 //
 //
 //
+import ProjectNavBar from '../components/ProjectNavBar';
+import ProjectHeader from '../components/ProjectHeader';
+import UpdateProjectInfo from '../components/UpdateProjectInfo';
+
+// Pass users array to the List component as a prop
+export default function EditInfo() {
+    return (
+        <div>
+            <div>
+                <ProjectNavBar />
+            </div>
+            <div>
+                <ProjectHeader />
+            </div>
+            <div>
+                <UpdateProjectInfo />
+            </div>
+        </div>
+    );
+}
