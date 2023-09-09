3f592d7c048396d1e43086233e0beea7d4040917
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-08 20:13:46 -0500

    queries.js - wireframe queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,24 +1,13 @@
-// ----------------------------------------------- modify code ----------------------------------------------- //
-
 import { gql } from "@apollo/client";
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+export const QUERY_WIREFRAME = gql`
+  query wireframe {
+    wireframe {
       _id
-      name
-    }
-  }
-`;
-
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      projectId
+      title
+      imageUrl
+      note
     }
   }
 `;
