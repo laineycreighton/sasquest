42996833fdeea0f37c4d004687a556da4d7dfb06
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-08 20:20:56 -0500

    mutations.js -wireframe

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,22 @@
 import { gql } from "@apollo/client";
 
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
+export const CREATE_WIREFRAME = gql`
+  mutation createWireframe(
+    $projectId: ID!
+    $title: String!
+    $imageUrl: String
+    $note: String
+  ) {
+    createWireframe(
+      projectId: $projectId
+      title: $title
+      imageUrl: $imageUrl
+      note: $note
+    ) {
       _id
-      tech1
-      tech2
-    }
-  }
-`;
-
-export const CREATE_VOTE = gql`
-  mutation createVote($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      projectId
+      imageUrl
+      note
     }
   }
 `;
