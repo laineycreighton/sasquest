f9d70d28a1758911379620c14a58b66cdc3ddb0b
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-09 19:57:07 -0500

    DisplayProjectInfo.jsx

--- client/src/components/DisplayProjectInfo.jsx
+++ client/src/components/DisplayProjectInfo.jsx
@@ -19,4 +19,48 @@
 //
 
 import { useEffect, useState } from "react";
-import { useMutation } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import { GET_PROJECT_BY_ID } from "../utils/queries";
+import { Link, useParams } from "react-router-dom";
+
+const DisplayProjectInfo = ({ projectID }) => {
+  const { id } = useParams();
+  const [project, setProject] = useState({});
+
+  const { loading, data } = useQuery(GET_PROJECT_BY_ID, {
+    variables: { projectID: id },
+  });
+
+  //
+  useEffect(() => {
+    if (data) {
+      setProject(data.project);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="project-info">
+      <div className="project-info-repo">
+        <h3>REPO URL</h3>
+        <p>{project.repoURL}</p>
+      </div>
+      <div className="project-info-deployed">
+        <h3>DEPLOYED URL</h3>
+        <p>{project.deployedURL}</p>
+      </div>
+      <div className="project-info-description">
+        <h3>DESCRIPTION</h3>
+        <p>{project.description}</p>
+      </div>
+      <Link to={`/edit/${project._id}`}>
+        <button className="project-info__edit-btn">EDIT</button>
+      </Link>
+    </div>
+  );
+};
+
+export default DisplayProjectInfo;
