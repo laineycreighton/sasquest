1c856b8614686a86346afde0d9b4130abf17712a
Lainey <dev.lainey@gmail.com>
2023-09-11 21:54:59 -0400

    add styling to home header

--- client/src/components/HomeHeader.jsx
+++ client/src/components/HomeHeader.jsx
@@ -1,21 +1,33 @@
 import { Link } from 'react-router-dom'
-import '../styles/HomeHeader.css';
+import sasquatch from '../assets/images/sasquatch.png'
+import '../assets/css/HomeHeader.css';
 
 //Export the HomeHeader page
 export default function HomeHeader() {
     return (
-        <div className='home-header'>
-            <div className='redirect-about'>
-                <Link to="/">Logout</Link>
+        <div className='full-home-header'>
+            <div className='home-header'>
+                <nav className='redirect-logout'>
+                    <Link to="/" className='logout'>Logout</Link>
+                </nav>
+                <section className='home-sasquest'>
+                    <div className='home-lg-letters'>
+                        <h1 className='homeLg'>S</h1>
+                    </div>
+                    <div className='home-logo-middle'>
+                        <h2 className='homeMiddle'>ASQUES</h2>
+                        <p className='home-tagline'>
+                            tame your beast
+                        </p>
+                    </div>
+                    <div className='home-lg-letters'>
+                        <h1 className='homeLg'>T</h1>
+                    </div>
+                </section>
             </div>
-            <div className='header'>
-                <header className='sasquest-name'>
-                    <h1><span>S</span>ASQUES<span>T</span></h1>
-                </header>
-                <p className='tagline'>
-                    tame your beast
-                </p>
+            <div className='home-mascot'>
+                <img src={sasquatch} alt='sasquatch' />
             </div>
-        </div>
+        </div >
     )
 }
