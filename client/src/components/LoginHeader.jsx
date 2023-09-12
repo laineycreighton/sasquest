e1aaa4dc46cbb6683e899d813b179edc1d13c6d6
Lainey <dev.lainey@gmail.com>
2023-09-11 20:04:48 -0400

    add sasquatch to login header

--- client/src/components/LoginHeader.jsx
+++ client/src/components/LoginHeader.jsx
@@ -1,27 +1,33 @@
 import { Link } from 'react-router-dom'
+import sasquatch from '../assets/images/sasquatch.png'
 import '../assets/css/LoginHeader.css'
 
 //Export the LoginHeader page
 export default function LoginHeader() {
     return (
-        <div className='login-header'>
-            <nav className='redirect-about'>
-                <Link to="/about" className='about'>About</Link>
-            </nav>
-            <section className='sasquest'>
-                <div className='lg-letters'>
-                    <h1>S</h1>
-                </div>
-                <div className='logo-middle'>
-                    <h2>ASQUES</h2>
-                    <p className='tagline'>
-                        tame your beast
-                    </p>
-                </div>
-                <div className='lg-letters'>
-                    <h1>T</h1>
-                </div>
-            </section>
+        <div className='full-header'>
+            <div className='mascot'>
+                <img src={sasquatch} alt='sasquatch' />
+            </div>
+            <div className='login-header'>
+                <nav className='redirect-about'>
+                    <Link to="/about" className='about'>About</Link>
+                </nav>
+                <section className='sasquest'>
+                    <div className='lg-letters'>
+                        <h1>S</h1>
+                    </div>
+                    <div className='logo-middle'>
+                        <h2>ASQUES</h2>
+                        <p className='tagline'>
+                            tame your beast
+                        </p>
+                    </div>
+                    <div className='lg-letters'>
+                        <h1>T</h1>
+                    </div>
+                </section>
+            </div>
         </div >
     )
 }
