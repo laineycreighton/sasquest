41254fa52d03b66d05c0d804ae7eab3e9ec7c4df
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-09-12 13:02:42 -0500

    sign-up form troubleshooting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,42 @@
 // header refers to metadata and other info that is within the HTTP request or response
-import './App.css';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-// import { setContext} from 'apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
 
 // graphQL API end route
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // middleware that attaches jwt token to header of each API call to authenticate user
-// const authLink = setContext((_, {headers}) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   // Gets client ready to run middleware before requesting anything from gql API
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  // Gets client ready to run middleware before requesting anything from gql API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return(
-    <div className='main'>
-        <Outlet />
-    </div>
-);}
+  return (
+    <ApolloProvider client={client}>
+      <Outlet />
+    </ApolloProvider>
+  );
+}
 
-export default App
+export default App;
