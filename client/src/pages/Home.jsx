e930a046298bcdc8c89ee2a534c051097ad96217
lukeharriman <lukeharriman1013@gmail.com>
2023-09-13 13:28:03 -0700

    home styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,19 @@
+import React from "react";
 import HomeHeader from "../components/HomeHeader";
 import ProjectDashboard from "../components/ProjectDashboard";
 import NewProject from "../components/NewProject";
+import AnimatedCursor from "react-animated-cursor"; // Import the AnimatedCursor component
 
 const Home = () => {
   return (
-    <div>
-      <HomeHeader />
-      <NewProject />
-      <ProjectDashboard />
-    </div>
+    // Wrap the entire component with AnimatedCursor
+    <AnimatedCursor>
+      <div>
+        <HomeHeader />
+        <NewProject />
+        <ProjectDashboard />
+      </div>
+    </AnimatedCursor>
   );
 };
 
