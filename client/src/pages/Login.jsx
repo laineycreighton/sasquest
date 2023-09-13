f8dec2937bbbb14dcad7947d0f029b4b24998466
lukeharriman <lukeharriman1013@gmail.com>
2023-09-13 13:28:42 -0700

    login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,17 @@
+import React from "react";
 import LoginHeader from "../components/LoginHeader";
 import LoginForm from "../components/LoginForm";
+import AnimatedCursor from "react-animated-cursor"; // Import the AnimatedCursor component
 
 // Pass users array to the List component as a prop
 export default function Login() {
   return (
-    <div>
+    // Wrap the entire component with AnimatedCursor
+    <AnimatedCursor>
+      <div>
         <LoginHeader />
         <LoginForm />
-    </div>
+      </div>
+    </AnimatedCursor>
   );
 }
