ec1ac7f748dd2dd529d3720a1f12b2465d64dadd
lukeharriman <lukeharriman1013@gmail.com>
2023-09-13 13:29:23 -0700

    signup form styling

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,20 @@
+import React from "react";
 import LoginHeader from "../components/LoginHeader";
 import SignUpForm from "../components/SignUpForm";
+import AnimatedCursor from "react-animated-cursor"; // Import the AnimatedCursor component
 
 export default function SignUp() {
   return (
-    <div>
+    // Wrap the entire component with AnimatedCursor
+    <AnimatedCursor>
       <div>
-        <LoginHeader />
+        <div>
+          <LoginHeader />
+        </div>
+        <div>
+          <SignUpForm />
+        </div>
       </div>
-      <div>
-        <SignUpForm />
-      </div>
-    </div>
+    </AnimatedCursor>
   );
 }
