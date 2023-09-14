10b95dfd4d9224c9009eeeb8d8178e2436cee560
lukeharriman <lukeharriman1013@gmail.com>
2023-09-13 21:43:58 -0700

    removing clutter

--- client/src/components/DisplayInfo.jsx
+++ client/src/components/DisplayInfo.jsx
@@ -5,12 +5,6 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT_BY_ID } from "../utils/queries";
 import { Link, useParams } from "react-router-dom";
-// DisplayProjectInfo component that takes projectID as a prop
-// When we click on a project, we want to display the project information
-// We will use the projectID to query the database for the project information
-// We will not have a form. The purpose of this is to access the project information
-// We are gonna access the deployed url, the title of the project, and the description, and the repo url
-// We will also have a button to edit the project information that links to UpdateInfo.jsx
 
 const DisplayProjectInfo = ({ projectID }) => {
   const { loading, error, data } = useQuery(GET_PROJECT_BY_ID, {
