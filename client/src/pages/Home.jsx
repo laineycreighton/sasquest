8485403e6c2775873ad13246675cd9cc2882ea9a
Lainey <dev.lainey@gmail.com>
2023-09-14 14:33:18 -0400

    add cursor to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import HomeHeader from "../components/HomeHeader";
-
+import AnimatedCursor from "react-animated-cursor"; // Import AnimatedCursor
 import NewProject from "../components/NewProject";
 
 
 const Home = () => {
   return (
     <div>
+      <AnimatedCursor /> {/* Add AnimatedCursor here */}
       <HomeHeader />
       <NewProject />
     </div>
