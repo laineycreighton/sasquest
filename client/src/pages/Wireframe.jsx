a17951d0a511722bcfc827d349f4546e45eeabc8
BohoBabe <slenorovitz@gmail.com>
2023-09-13 23:11:41 -0700

    updated cureser in wirefream

--- client/src/pages/Wireframe.jsx
+++ client/src/pages/Wireframe.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ProjectNavBar from "../components/ProjectNavBar";
 import ProjectHeader from "../components/ProjectHeader";
 import WireframeDashboard from "../components/WireframeDashboard";
+import AnimatedCursor from "react-animated-cursor"; // Import the AnimatedCursor component
 
 const Wireframe = () => {
   return (
     <div>
+      <AnimatedCursor /> {/* Add AnimatedCursor here */}
       <ProjectNavBar />
       {/* <ProjectHeader /> */}
       <WireframeDashboard />
