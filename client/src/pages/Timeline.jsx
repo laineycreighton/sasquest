159d9e90670fc8b469c6166047ade310575a0d6c
BohoBabe <slenorovitz@gmail.com>
2023-09-13 23:15:39 -0700

    added curser to timeline

--- client/src/pages/Timeline.jsx
+++ client/src/pages/Timeline.jsx
@@ -2,11 +2,13 @@ import ProjectNavBar from "../components/ProjectNavBar.jsx";
 import ViewTimeline from "../components/ViewTimeline.jsx";
 import AddTimeline from "../components/AddTimeline.jsx";
 import React from "react";
+import AnimatedCursor from "react-animated-cursor"; // Import the AnimatedCursor component
 
 const Timeline = () => {
 
   return (
     <div>
+      <AnimatedCursor /> {/* Add AnimatedCursor here */}
       <ProjectNavBar />
       <AddTimeline />
       <ViewTimeline />
